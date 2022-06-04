aa436a307d2adadada29e899f197e2535c54d439
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-06-04 14:42:04 +0300

    commit tree 7cbe0f2c5aac3ceb141428258ae110668cd03874

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const employee = {};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    updateEmployeeWithKeyAndValue = {...employee};
+updateEmployeeWithKeyAndValue[key] = value;
+return updateEmployeeWithKeyAndValue;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee}
+    delete newEmployee[key];
+    return newEmployee;
+}
